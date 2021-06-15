82245563309afdd2be544b8e20a25369771d5513
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-06-15 19:41:08 -0400

    clicking the button updates the time using states.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+// let buttonClicked = false;
+
+// function getTime () {
+//   buttonClicked = true;
+// }
+
+// function getTime () {
+//   setDisplayedTime(displayedTime = time)
+// }
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+  let [displayedTime, setDisplayedTime] = useState(time);
+
+  function getTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setDisplayedTime(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{displayedTime}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
